a2af17d8fafb117fce3e5fd24b6e9026173bd40d
Egbudom-Raphael <egbudomraphael@gmail.com>
2023-03-15 23:48:57 +0000

    change machine learning model

--- fast_api.py
+++ fast_api.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from tensorflow import keras
 from typing import Dict
+import numpy as np
+import pickle
 # import json
 import pandas as pd
 
@@ -22,13 +24,16 @@ app.add_middleware(
 def predict(data: Dict):
     # Convert the input data to a pandas DataFrame
     df = pd.DataFrame.from_dict(data, orient='index').T
-
+    print(df)
     # loading the saved model
-    model = keras.models.load_model('1d_conv.hdf5', compile=False)
+    # model=pickle.load(open('1d_conv.pkl','rb'))
+    model = keras.models.load_model('lstm.hdf5', compile=False)
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # Make a prediction using the trained model
-    y_pred = model.predict(df)
+    inp = np.asarray(df)
+    input_data = inp.reshape(-1,1, 28)
+    y_pred = model.predict(input_data)
     print(y_pred)
     # Return the prediction as a dictionary
     return f'{y_pred[0][0]}'
